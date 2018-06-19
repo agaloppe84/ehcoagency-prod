4b191e4fc2b22ae0095c383e42284f1ec02c3867
aurelien galoppe <agaloppe@me.com>
2018-06-19 17:45:06 +0200

    init commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,16 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :services, :references, :contact]
 
   def home
   end
+
+  def services
+  end
+
+  def references
+  end
+
+  def contact
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'contact', to: 'pages#contact'
+  get 'references', to: 'pages#references'
+  get 'services', to: 'pages#services'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
